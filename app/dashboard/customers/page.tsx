247cbb70269a4d8fa7eb42e589269f3e6d9103f7
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-02 20:45:01 +0800

    Adding layout and basic routes

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+const CustomerPage = () => {
+  return <p>CustomerPage</p>
+}
+
+export default CustomerPage
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+const InvoicesPage = () => {
+  return <p>InvoicesPage</p>
+}
+
+export default InvoicesPage
