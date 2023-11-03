764b79b17763fce41767b1ff50541b6ef02c9ec7
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-03 15:31:47 +0800

    Put the type back in

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -10,14 +10,18 @@ import { fetchRevenue } from '@/app/lib/data'
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart() {
-  const revenue = await fetchRevenue()
+export default async function RevenueChart({
+  revenue,
+}: {
+  revenue?: Revenue[]
+}) {
+  const revenuedata = await fetchRevenue()
   const chartHeight = 350
   // NOTE: comment in this code when you get to this point in the course
 
-  const { yAxisLabels, topLabel } = generateYAxis(revenue)
+  const { yAxisLabels, topLabel } = generateYAxis(revenuedata)
 
-  if (!revenue || revenue.length === 0) {
+  if (!revenuedata || revenuedata.length === 0) {
     return <p className="mt-4 text-gray-400">No data available.</p>
   }
 
@@ -39,7 +43,7 @@ export default async function RevenueChart() {
             ))}
           </div>
 
-          {revenue.map((month) => (
+          {revenuedata.map((month) => (
             <div
               key={month.month}
               className="flex flex-col items-center gap-2"
@@ -47,7 +51,7 @@ export default async function RevenueChart() {
               <div
                 className="w-full rounded-md bg-blue-300"
                 style={{
-                  height: `${(chartHeight / topLabel) * month.revenue}px`,
+                  height: `${(chartHeight / topLabel) * month.revenuedata}px`,
                 }}
               ></div>
               <p className="-rotate-90 text-sm text-gray-400 sm:rotate-0">
