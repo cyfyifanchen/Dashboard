94ee79c00c5d53a4bf5b15c8d107921deacd3e1e
贝吉塔大王 <cyfyifanchen@gmail.com>
2023-11-05 17:38:48 +0800

    auth and

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,7 @@ import { z } from 'zod'
 import { sql } from '@vercel/postgres'
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
+import { signIn } from '@/auth'
 
 const InvoiceSchema = z.object({
   id: z.string(),
@@ -29,6 +30,20 @@ export type State = {
     status?: string[]
   }
   message?: string | null
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData))
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialSignin'
+    }
+    throw error
+  }
 }
 
 export async function createInvoice(prevState: State, formData: FormData) {
